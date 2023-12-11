915ae4553e8e895f490f1ad591721d1133928d59
vietqtran <tranquocviet1303@gmail.com>
2023-12-11 23:08:02 +0700

    update 4 files and create 8 files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Income from './components/Income'
+import Login from './components/Login'
+import Spending from './components/Spending'
+import { useState } from 'react'
+
 function App() {
+  const [ tab, setTab ] = useState("login")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {tab === 'income' && <Income setTab={setTab} />}
+      {tab === 'spending' && <Spending setTab={setTab} />}
+      {tab === 'login' && <Login />}
     </div>
   );
 }
